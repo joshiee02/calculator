e8f5bd798d535d55ffa355354ea7d50382b58c0e
jk <jkhirakosocmed@gmail.com>
2022-12-24 17:50:31 +0900

    add function for calculating math

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let add = (num1, num2) => num1 + num2;
 let subtract = (num1, num2) => num1 - num2;
 let multiply = (num1, num2) => num1 * num2;
 let divide = (num1, num2) => num1 / num2;
+
+function operator(operator, num1, num2){
+    return operator(num1, num2);
+}
