940748373a2bf36bc3db37baf138c4bf4f4daafc
jk <jkhirakosocmed@gmail.com>
2022-12-25 14:19:20 +0900

    optimized querySelector in buttons, add screen display func

--- script.js
+++ script.js
@@ -3,24 +3,27 @@ let subtract = (num1, num2) => num1 - num2;
 let multiply = (num1, num2) => num1 * num2;
 let divide = (num1, num2) => num1 / num2;
 
-function operator(operator, num1, num2){
-    return operator(num1, num2);
+function operator(calc){
+    return +calc
 }
+let currentValue = '';
+
 const displayValue = document.querySelector('#displayValue');
-const num1 = document.querySelector('.num1');
-const num2 = document.querySelector('.num2');
-const num3 = document.querySelector('.num3');
-const num4 = document.querySelector('.num4');
-const num5 = document.querySelector('.num5');
-const num6 = document.querySelector('.num6');
-const num7 = document.querySelector('.num7');
-const num8 = document.querySelector('.num8');
-const num9 = document.querySelector('.num9');
+const allButtons = document.querySelectorAll('.buttons');
+    allButtons.forEach(button => button.onclick = () => {
+        currentValue += button.value
+        displayValue.textContent = currentValue;
+    });
 const numAddition = document.querySelector('.numAddition');
 const numSubtract = document.querySelector('.numSubtract');
 const numMultiply = document.querySelector('.numMultiply');
 const numDivide = document.querySelector('.numDivide');
 const numEquals = document.querySelector('.numEquals');
 const numDel = document.querySelector('.numDel');
+    numDel.onclick = () => {
+        currentValue = '';
+        displayValue.textContent = currentValue;
+    };
+
 
 
