f23fe69cd994202a0e2a403bb100ef3cbcd5fbfe
jk <jkhirakosocmed@gmail.com>
2022-12-25 12:33:55 +0900

    add query selector for buttons

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ let divide = (num1, num2) => num1 / num2;
 function operator(operator, num1, num2){
     return operator(num1, num2);
 }
+
+const num1 = document.querySelector('.num1');
+const num2 = document.querySelector('.num2');
+const num3 = document.querySelector('.num3');
+const num4 = document.querySelector('.num4');
+const num5 = document.querySelector('.num5');
+const num6 = document.querySelector('.num6');
+const num7 = document.querySelector('.num7');
+const num8 = document.querySelector('.num8');
+const num9 = document.querySelector('.num9');
+const numAddition = document.querySelector('.numAddition');
+const numSubtract = document.querySelector('.numSubtract');
+const numMultiply = document.querySelector('.numMultiply');
+const numDivide = document.querySelector('.numDivide');
+const numEquals = document.querySelector('.numEquals');
+const numDel = document.querySelector('.numDel');
+
