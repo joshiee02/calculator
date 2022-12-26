2bbccf86e64f9463090a95325c3c03f7e388e528
jk <jkhirakosocmed@gmail.com>
2022-12-26 21:51:12 +0900

    add functionality to calculator, bugs not fixed

--- script.js
+++ script.js
@@ -2,27 +2,59 @@ let add = (x, y) => x + y;
 let subtract = (x, y) => x - y;
 let multiply = (x, y) => x * y;
 let divide = (x, y) => x / y;
+let calcResult = 0;
 
-function operator(calc, firstNum, secondNum){
-    return calc(firstNum, secondNum);
+
+function operate(calc, firstNum, secondNum){
+    calcResult = calc(firstNum, secondNum);
 }
-let currentValue = '';
 
-const displayValue = document.querySelector('#displayValue');
-const allButtons = document.querySelectorAll('.buttons');
-    allButtons.forEach(button => button.onclick = () => {
-        currentValue += button.value
-        displayValue.textContent = currentValue;
+let firstValue = '';
+let operatorValue ='';
+let secondValue ='';
+
+let displayValue = '';
+const display = document.querySelector('#displayValue');
+const numButtons = document.querySelectorAll('.buttons');
+    numButtons.forEach(button => button.onclick = () => {
+        displayValue += button.value;
+        display.textContent = displayValue;
+        if(operatorValue){
+            secondValue += +displayValue;
+        }else firstValue += +displayValue;
     });
-const numAddition = document.querySelector('.numAddition');
-const numSubtract = document.querySelector('.numSubtract');
-const numMultiply = document.querySelector('.numMultiply');
-const numDivide = document.querySelector('.numDivide');
+
+const operatorButtons = document.querySelectorAll('.operatorButtons')
+    operatorButtons.forEach(operator => operator.onclick = () => {
+        if(!operatorValue){
+            operatorValue += operator.value;
+        }else{
+            operatorValue = '';
+            operatorValue += operator.value;
+        }
+        displayValue = '';   
+});
 const numEquals = document.querySelector('.numEquals');
+    numEquals.onclick = () => {
+        if(operatorValue == 'add'){
+            operate(add, +firstValue, +secondValue);
+            display.textContent = calcResult;
+        }else if(operatorValue == 'subtract'){
+            operate(subtract, +firstValue, +secondValue);
+            display.textContent = calcResult;
+        }else if(operatorValue == 'multiply'){
+            operate(multiply, +firstValue, +secondValue);
+            display.textContent = calcResult;
+        }else if(operatorValue == 'divide'){
+            operate(divide, +firstValue, +secondValue);
+            display.textContent = calcResult;
+        }
+    };
+
 const numDel = document.querySelector('.numDel');
     numDel.onclick = () => {
-        currentValue = '';
-        displayValue.textContent = currentValue;
+        displayValue = '';
+        display.textContent = displayValue;
     };
 
 
