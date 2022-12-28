e38942267f2a8faea76a9ff837e3529ae22ba616
jk <jkhirakosocmed@gmail.com>
2022-12-28 20:15:31 +0900

    add functionality to calculator

--- script.js
+++ script.js
@@ -2,31 +2,54 @@ let add = (x, y) => x + y;
 let subtract = (x, y) => x - y;
 let multiply = (x, y) => x * y;
 let divide = (x, y) => x / y;
+
 let calcResult = 0;
-
-
-function operate(calc, firstNum, secondNum){
-    calcResult = calc(firstNum, secondNum);
-}
-
 let firstValue = '';
 let operatorValue ='';
 let secondValue ='';
 
 let displayValue = '';
+
+
+function operate(calc, firstNum, secondNum){
+    calcResult = calc(firstNum, secondNum);
+    displayValue = calcResult;
+    display.textContent = displayValue;
+}
+
 const display = document.querySelector('#displayValue');
 const numButtons = document.querySelectorAll('.buttons');
     numButtons.forEach(button => button.onclick = () => {
         displayValue += button.value;
         display.textContent = displayValue;
         if(operatorValue){
-            secondValue += +displayValue;
-        }else firstValue += +displayValue;
+            secondValue = displayValue;
+        }else firstValue = displayValue;
     });
 
 const operatorButtons = document.querySelectorAll('.operatorButtons')
     operatorButtons.forEach(operator => operator.onclick = () => {
-        if(!operatorValue){
+        if(operatorValue ='add' && firstValue && secondValue){
+            if(calcResult) {
+                operate(add, calcResult, +secondValue);
+            }else operate(add, +firstValue, +secondValue);
+        }
+        else if(operatorValue ='subtract' && firstValue && secondValue){
+            if(calcResult) {
+                operate(subtract, calcResult, +secondValue);
+            }else operate(subtract, +firstValue, +secondValue);
+        }
+        else if(operatorValue ='multiply' && firstValue && secondValue){
+            if(calcResult) {
+                operate(multiply, calcResult, +secondValue);
+            }else operate(multiply, +firstValue, +secondValue);
+        }
+        else if(operatorValue ='divide' && firstValue && secondValue){
+            if(calcResult) {
+                operate(divide, calcResult, +secondValue);
+            }else operate(divide, +firstValue, +secondValue);
+        }
+        else if(!operatorValue){
             operatorValue += operator.value;
         }else{
             operatorValue = '';
@@ -37,24 +60,35 @@ const operatorButtons = document.querySelectorAll('.operatorButtons')
 const numEquals = document.querySelector('.numEquals');
     numEquals.onclick = () => {
         if(operatorValue == 'add'){
-            operate(add, +firstValue, +secondValue);
-            display.textContent = calcResult;
+            if(calcResult) {
+                operate(add, calcResult, +secondValue);
+            }else operate(add, +firstValue, +secondValue);
         }else if(operatorValue == 'subtract'){
-            operate(subtract, +firstValue, +secondValue);
-            display.textContent = calcResult;
+            if(calcResult) {
+                operate(subtract, calcResult, +secondValue);
+            }else operate(subtract, +firstValue, +secondValue);
         }else if(operatorValue == 'multiply'){
-            operate(multiply, +firstValue, +secondValue);
-            display.textContent = calcResult;
+            if(calcResult) {
+                operate(multiply, calcResult, +secondValue);
+            }else operate(multiply, +firstValue, +secondValue);
         }else if(operatorValue == 'divide'){
-            operate(divide, +firstValue, +secondValue);
-            display.textContent = calcResult;
+            if(calcResult) {
+                operate(divide, calcResult, +secondValue);
+            }else operate(divide, +firstValue, +secondValue);
         }
     };
 
 const numDel = document.querySelector('.numDel');
     numDel.onclick = () => {
+
+        //reset 
         displayValue = '';
         display.textContent = displayValue;
+        firstValue = '';
+        secondValue = '';
+        operatorValue = '';
+        calcResult = 0;
+
     };
 
 
